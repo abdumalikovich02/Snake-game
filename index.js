63bde751ba8ce1d1e5903390664790dcf9af427d
abdumalikovich02 <abdulaxad1909@gmail.com>
2022-03-10 09:50:03 +0500

    js

---
+++ index.js
@@ -0,0 +1,36 @@
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+let speed = 7;
+
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+let headX = 10;
+let headY = 10;
+
+
+let xVelocity = 0;
+let yVelocity = 0;
+
+
+
+// game loop
+function drawGame() {
+    clearScreen();
+    drawSnake();
+    setTimeout(drawGame, 1000 / speed);
+}
+
+function clearScreen() {
+    ctx.fillStyle = 'black';
+    ctx.fillReact(0, 0, canvas.width, canvas.height);
+
+}
+
+function drawSnake() {
+    ctx.fillStyle = 'orange';
+    ctx.fillReact(headX * tileCount, headY * tileCount, tileSize, tileSize);
+
+}
+
+drawGame();
